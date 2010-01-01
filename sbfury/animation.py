8f7ac54822f9118ba6d4f85a77a35c2a0d3e1886
hugoruscitti <hugoruscitti@localhost>
2010-01-01 19:00:53 -0300

    agregando animaciones al protagonista.

---
+++ sbfury/animation.py
@@ -0,0 +1,44 @@
+# License: Public Domain
+# Author: Hugo Ruscitti (http://www.losersjuegos.com.ar)
+import sheet
+
+class Animation:
+
+    def __init__(self, sheet, delay, sequence, stop_when_finish=False):
+        self.step_in_sequence = 0
+        self.sheet = sheet
+        self.delay = delay
+        self.reset()
+        self.sequence = sequence
+        self.stop_when_finish = stop_when_finish
+
+    def reset(self):
+        self.delay_counter = 0
+
+    def update(self, dt):
+        "Avanza en la animacion y retorna True si ha reiniciado."
+        self.delay_counter += dt
+
+        if self.delay_counter >= self.delay:
+            self.reset()
+            return self.next_frame()
+
+    def Assign(self, sprite):
+        self.sheet.Assign(sprite)
+
+    def next_frame(self):
+        "Avanza en la secuencia y retorna True si la animacion termina."
+        was_restarted = False
+        self.step_in_sequence += 1
+
+        if self.step_in_sequence >= len(self.sequence):
+            if self.stop_when_finish:
+                self.step_in_sequence -= 1
+            else:
+                self.step_in_sequence = 0
+                was_restarted = True
+
+        frame_to_show = self.sequence[self.step_in_sequence]
+        self.sheet.SetFrameIndex(frame_to_show)
+
+        return was_restarted
--- sbfury/shaolin.py
+++ sbfury/shaolin.py
@@ -1,5 +1,6 @@
 from PySFML import sf
 import common
+import animation
 
 class Shaolin(sf.Sprite):
 
@@ -9,6 +10,23 @@ class Shaolin(sf.Sprite):
         image.Assign(self)
         self.image = image
         self.SetPosition(200, 200)
+        self._load_animations()
+        self.set_animation('stand')
+
+    def _load_animations(self):
+
+        image = common.load_sheet("../data/shaolin/stand.png", 4, 1)
+        
+        self.animations = {
+                'stand': animation.Animation(image, 0.20, [0, 1, 2, 3]),
+                }
+
+    def set_animation(self, animation_name):
+        self.animation = self.animations[animation_name]
 
     def update(self, dt):
-        pass
+        self.update_animation(dt)
+
+    def update_animation(self, dt):
+        self.animation.update(dt)
+        self.animation.Assign(self)
